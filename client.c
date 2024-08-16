9d0ec32c85b3d1cf0c614733582d5dd197fbc0a6
Yannis Mauk <ymauk@2-C-2.42heilbronn.de>
2024-08-16 10:58:04 +0200

    16.08.2024

--- client.c
+++ client.c
@@ -6,12 +6,11 @@
 /*   By: ymauk <ymauk@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/13 15:44:16 by ymauk             #+#    #+#             */
-/*   Updated: 2024/08/14 15:55:36 by ymauk            ###   ########.fr       */
+/*   Updated: 2024/08/16 10:03:46 by ymauk            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
-#include <stdio.h>
 
 void	sending_message(pid_t sending_pid, char *message)
 {
@@ -28,10 +27,11 @@ void	sending_message(pid_t sending_pid, char *message)
 		x = 0;
 		while (x < 8)
 		{
-			if (c &(1 << x) != 0)
+			if ((c & (1 << x)) != 0)
 				kill (sending_pid, SIGUSR1);
 			else
 				kill (sending_pid, SIGUSR2);
+			usleep(100);
 			x++;
 		}
 		i++;
@@ -43,7 +43,7 @@ int	main(int argc, char **argv)
 	pid_t	sending_pid;
 
 	if (argc != 3)
-		return (ft_printf("Wrong amount of arguments!"));
+		return (ft_printf("%sWrong amount of arguments!%s\n", R, DC));
 	sending_pid = ft_atoi(argv[1]);
 	sending_message(sending_pid, argv[2]);
 	return (0);
--- server.c
+++ server.c
@@ -6,19 +6,60 @@
 /*   By: ymauk <ymauk@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/13 15:44:24 by ymauk             #+#    #+#             */
-/*   Updated: 2024/08/14 14:30:20 by ymauk            ###   ########.fr       */
+/*   Updated: 2024/08/16 10:56:29 by ymauk            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+char	*character_to_string(char *string, char character)
+{
+	
+}
+
+void	handle_signal(int signal)
+{
+	static char	character;
+	static int	pos_bit;
+	static char	*string = NULL;
+
+	pos_bit = 0;
+	character = 0;
+	if (signal == SIGUSR1)
+		character |= (1 << pos_bit);
+	pos_bit++;
+	if (pos_bit == 8)
+	{
+		if (character != '\0')
+			character_to_string(&string, character);
+		else
+		{
+			ft_printf("%s", string);
+			free (string);
+			string = NULL;
+		}
+	}
+	pos_bit = 0;
+	character = 0;
+}
+
 int	main(int argc, char **argv)
 {
-	int	pid;
+	struct sigaction	sa;
+	int					pid;
 
-	argv = 0;
+	argv = NULL;
 	argc = 0;
 	pid = getpid();
 	ft_printf("%sPID Number: %d\n%s", G, pid, DC);
+	sa.sa_handler = handle_signal;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_RESTART;
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
+	while (1)
+	{
+		pause();
+	}
 	return (0);
 }
